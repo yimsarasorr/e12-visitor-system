8171749890b25e4f60c52d8b4cdcd155834244b9
yimsarasorr <yimsara000@gmail.com>
2025-08-29 21:28:39 +0700

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('e12-visitor-system');
+}
