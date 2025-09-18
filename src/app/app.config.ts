7c5c9b0803415e27c30e1b13c254cf849b841248
yimsarasorr <yimsara000@gmail.com>
2025-09-18 22:09:07 +0700

    Add Angular Router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
-import Lara from '@primeng/themes/lara';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideAnimationsAsync(),
+    provideRouter(routes),
+    provideAnimations(),
     providePrimeNG({
-      theme: {
-        preset: Lara,
-      },
+        theme: {
+            preset: Aura,
+        },
     }),
-  ],
+  ]
 };
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { FloorPlanComponent } from './components/floor-plan/floor-plan.component';
+
+export const routes: Routes = [
+    { path: '', component: FloorPlanComponent }
+];
